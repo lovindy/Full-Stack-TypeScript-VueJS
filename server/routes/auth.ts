d3d8042fb96c9ddabc6479a4cdbda4ef10d56119
Seang Makaramarady <seangdy085@gmail.com>
2024-11-23 15:33:53 +0700

    feature: add the route of authentication

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,10 +1,22 @@
 // src/routes/auth.ts
 import { Router } from "express";
-import { register, login } from "../controllers/authController";
+import {
+  register,
+  login,
+  logout,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/authController";
+import { protect } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", protect, logout);
+router.put("/change-password", protect, changePassword);
+router.post("/forgot-password", forgotPassword);
+router.put("/reset-password", resetPassword);
 
 export default router;
